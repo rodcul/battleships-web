12e2a22bfdee22a300f3aea61fd256c243e000da
Rodney Cullen <rodcul@gmail.com>
2015-05-11 22:20:39 +0100

    [UNFINISHED] Version 1: Building an application from the outside in

---
+++ lib/battleships.rb
@@ -0,0 +1,20 @@
+require 'sinatra/base'
+
+
+class BattleShips < Sinatra::Base
+
+  set :views, Proc.new { File.join(root, "..", "views") }
+
+
+  get '/' do
+    erb :index
+
+  end
+
+  get '/game/new' do
+    erb :new_game
+  end
+
+  # start the server if ruby file executed directly
+  run! if app_file == $0
+end
